86ed13891f9fa9cc461c33df87a651aded4a062d
hehelego <hahalegao@gmail.com>
2024-02-12 11:10:40 +0800

    scanner test coverage: keywords, symbols

--- test/Test.hs
+++ test/Test.hs
@@ -6,18 +6,26 @@ import Test.Hspec
 
 crng x0 y0 x1 y1 = CodeRng (CodeLoc x0 y0) (CodeLoc x1 y1)
 
+crng1 y0 = crng 1 y0 1
+
 main :: IO ()
 main = hspec $ do
   describe "Scanner" $ do
     it "returns EOF for empty input" $ do
-      scan "" `shouldBe` (Right (), [Token "" (crng 1 1 1 1) EOF])
+      scan "" `shouldBe` (Right (), [Token "" (crng1 1 1) EOF])
+
+    it "skip comment" $ do
+      scan "\t\n  //hello world" `shouldBe` (Right (), [Token "" (crng 2 16 2 16) EOF])
+
+    it "reject unexpected character" $ do
+      scan "_invalid" `shouldBe` (Left $ ScanErr "unexpected char at 1:1", [])
 
     it "identify string literal and escape" $ do
       let raw = "\"\\bhe\\\"llo\\n\"" -- "\bhe\"llo\n"
       scan raw
         `shouldBe` ( Right (),
-                     [ Token "\"\bhe\"llo\n\"" (crng 1 1 1 13) STRING,
-                       Token "" (crng 1 14 1 14) EOF
+                     [ Token "\"\bhe\"llo\n\"" (crng1 1 13) STRING,
+                       Token "" (crng1 14 14) EOF
                      ]
                    )
 
@@ -26,17 +34,56 @@ main = hspec $ do
       scan raw `shouldBe` (Left $ ScanErr "unexpected EOF at 1:25", [])
 
     it "find numerical tokens && math operators" $ do
-      scan "123. >= 0.5  7 < 4 + 10"
+      scan "123. >= 0.5  7 < 4 + 10>!.,;/-{}(*)!===<="
         `shouldBe` ( Right (),
-                     [ Token "123." (crng 1 1 1 4) NUMBER,
-                       Token ">=" (crng 1 6 1 7) GREATER_EQUAL,
-                       Token "0.5" (crng 1 9 1 11) NUMBER,
-                       Token "7" (crng 1 14 1 14) NUMBER,
-                       Token "<" (crng 1 16 1 16) LESS,
-                       Token "4" (crng 1 18 1 18) NUMBER,
-                       Token "+" (crng 1 20 1 20) PLUS,
-                       Token "10" (crng 1 22 1 23) NUMBER,
-                       Token "" (crng 1 24 1 24) EOF
+                     [ Token "123." (crng1 1 4) NUMBER,
+                       Token ">=" (crng1 6 7) GREATER_EQUAL,
+                       Token "0.5" (crng1 9 11) NUMBER,
+                       Token "7" (crng1 14 14) NUMBER,
+                       Token "<" (crng1 16 16) LESS,
+                       Token "4" (crng1 18 18) NUMBER,
+                       Token "+" (crng1 20 20) PLUS,
+                       Token "10" (crng1 22 23) NUMBER,
+                       Token ">" (crng1 24 24) GREATER,
+                       Token "!" (crng1 25 25) BANG,
+                       Token "." (crng1 26 26) DOT,
+                       Token "," (crng1 27 27) COMMA,
+                       Token ";" (crng1 28 28) SEMICOLON,
+                       Token "/" (crng1 29 29) SLASH,
+                       Token "-" (crng1 30 30) MINUS,
+                       Token "{" (crng1 31 31) LEFT_BRACE,
+                       Token "}" (crng1 32 32) RIGHT_BRACE,
+                       Token "(" (crng1 33 33) LEFT_PAREN,
+                       Token "*" (crng1 34 34) STAR,
+                       Token ")" (crng1 35 35) RIGHT_PAREN,
+                       Token "!=" (crng1 36 37) BANG_EQUAL,
+                       Token "==" (crng1 38 39) EQUAL_EQUAL,
+                       Token "<=" (crng1 40 41) LESS_EQUAL,
+                       Token "" (crng1 42 42) EOF
+                     ]
+                   )
+
+    it "recognize keywords" $ do
+      let raw = "class else false for fun if nil not or print return super this true var while"
+      scan raw
+        `shouldBe` ( Right (),
+                     [ Token "class" (crng1 1 5) CLASS,
+                       Token "else" (crng1 7 10) ELSE,
+                       Token "false" (crng1 12 16) FALSE,
+                       Token "for" (crng1 18 20) FOR,
+                       Token "fun" (crng1 22 24) FUN,
+                       Token "if" (crng1 26 27) IF,
+                       Token "nil" (crng1 29 31) NIL,
+                       Token "not" (crng1 33 35) NOT,
+                       Token "or" (crng1 37 38) OR,
+                       Token "print" (crng1 40 44) PRINT,
+                       Token "return" (crng1 46 51) RETURN,
+                       Token "super" (crng1 53 57) SUPER,
+                       Token "this" (crng1 59 62) THIS,
+                       Token "true" (crng1 64 67) TRUE,
+                       Token "var" (crng1 69 71) VAR,
+                       Token "while" (crng1 73 77) WHILE,
+                       Token "" (crng1 78 78) EOF
                      ]
                    )
 
